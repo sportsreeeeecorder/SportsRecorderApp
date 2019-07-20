dff9024b992713760e8f15943c59722f24623fed
Takumi <takumi.dawn@gmail.com>
2019-07-20 18:09:11 -0500

    record page

--- SportsRecorder/app/src/main/java/edu/illinois/cs465/sportsrecorder/RecordGameActivity.java
+++ SportsRecorder/app/src/main/java/edu/illinois/cs465/sportsrecorder/RecordGameActivity.java
@@ -1,7 +1,12 @@
 package edu.illinois.cs465.sportsrecorder;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class RecordGameActivity extends Activity {
 
@@ -10,5 +15,19 @@ public class RecordGameActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_game);
         getActionBar().hide();
+
+//        File imgFile = new File("drawable-v24/basketballcourt.jpg");
+//        if(imgFile.exists()){
+//
+//            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+//
+//            ImageView myImage = (ImageView) findViewById(R.id.courtPic);
+//
+//            myImage.setImageBitmap(myBitmap);
+//
+//        }
+
     }
+
+
 }
